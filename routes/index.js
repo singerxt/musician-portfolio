91cc46fb3ecb32012a71ce115db75cd1a3f7eae7
singerxt <mat.spiewak@gmail.com>
2015-02-23 15:56:41 +0100

    reindent

--- routes/index.js
+++ routes/index.js
@@ -19,8 +19,8 @@
  */
 
 var keystone = require('keystone'),
-	middleware = require('./middleware'),
-	importRoutes = keystone.importer(__dirname);
+    middleware = require('./middleware'),
+    importRoutes = keystone.importer(__dirname);
 
 // Common Middleware
 keystone.pre('routes', middleware.initLocals);
@@ -28,17 +28,17 @@ keystone.pre('render', middleware.flashMessages);
 
 // Import Route Controllers
 var routes = {
-	views: importRoutes('./views')
+  views: importRoutes('./views')
 };
 
 // Setup Route Bindings
 exports = module.exports = function(app) {
-	
-	// Views
-	app.get('/', routes.views.index);
-	
-	
-	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
-	// app.get('/protected', middleware.requireUser, routes.views.protected);
-	
+
+  // Views
+  app.get('/', routes.views.index);
+
+
+  // NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
+  // app.get('/protected', middleware.requireUser, routes.views.protected);
+
 };
--- routes/views/index.js
+++ routes/views/index.js
@@ -15,56 +15,56 @@ exports = module.exports = function(req, res) {
    */
 
   view.on('init', function (next) {
-      keystone.list('header').model.find().exec(function (err,results) {
-        if (err || !results.length) {
-          return next(err);
-		}
+    keystone.list('header').model.find().exec(function (err,results) {
+      if (err || !results.length) {
+        return next(err);
+      }
 
-        locals.title = results[0].title;
-        locals.subTitle = results[0].subTitle;
-        locals.tags = results[0].tags;
-        next();
-      });
+      locals.title = results[0].title;
+      locals.subTitle = results[0].subTitle;
+      locals.tags = results[0].tags;
+      next();
+    });
   });
-  
+
   /*
    * Videos configuration
    */
-  
-  view.on('init', function (next) {
-      keystone.list('videos').model.find().exec(function (err,results) {
-        if (err || !results.length) {
-          return next(err);
-		}
 
-        locals.videos = results;
-        next();
-      });
+  view.on('init', function (next) {
+    keystone.list('videos').model.find().exec(function (err,results) {
+      if (err || !results.length) {
+        return next(err);
+      }
+
+      locals.videos = results;
+      next();
+    });
   });
-  
+
   /*
    * Listen configuration
    */
-  
-  view.on('init', function (next) {
-      keystone.list('listen').model.find().exec(function (err,results) {
-        if (err || !results.length) {
-          return next(err);
-		}
-        locals.description = results[0].description;
-        next();
-      });
-  });
-  
-  view.on('init', function (next) {
-      keystone.list('soundCloud').model.find().exec(function (err,results) {
-        if (err || !results.length) {
-          return next(err);
-		}
 
-        locals.soundCloud = results;
-        next();
-      });
+  view.on('init', function (next) {
+    keystone.list('listen').model.find().exec(function (err,results) {
+      if (err || !results.length) {
+        return next(err);
+      }
+      locals.description = results[0].description;
+      next();
+    });
+  });
+
+  view.on('init', function (next) {
+    keystone.list('soundCloud').model.find().exec(function (err,results) {
+      if (err || !results.length) {
+        return next(err);
+      }
+
+      locals.soundCloud = results;
+      next();
+    });
   });
 
   // Render the view
