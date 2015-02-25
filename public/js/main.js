d394dbb66702e633ae272f06975f20601a76a951
singerxt <mat.spiewak@gmail.com>
2015-02-25 02:12:55 +0100

    added videos rotator

--- public/js/main.js
+++ public/js/main.js
@@ -103,11 +103,91 @@
       video: $('.video'),
       iframe: $('.video-poput iframe'),
       poput: $('.video-poput'),
-      videoContainer: $('.videos-list')
+      videoContainer: $('.videos-list'),
+      track: $('.videos-track'),
+      wayPoints: [],
+      currentIndex: 0,
+      controls: $('.controls a'),
+      slidesToShow: function () {
+        var mobile = 1,
+            tablet = 3,
+            desktop = 4;
+        if (window.innerWidth <= 639) {
+          return mobile;
+        } else if (window.innerWidth > 639 && window.innerWidth <= 1024) {
+          return tablet;
+        } else if (window.innerWidth > 1024) {
+          return desktop;
+        }
+      }
     },
     
-    initRotator: function () {
+    bindResizeEvent: function () {
+      var $window = $(window),
+          that = this;
+      
+      $window.on('resize', function () {
+        that.setTrackWidth();
+      });
+    },
 
+    setTrackWidth: function () {
+      var howMuchSlides = this.config.video.length,
+          divider = this.config.slidesToShow(),
+          slideWidth = parseInt(window.innerWidth / divider,10);
+      
+      this.config.video.css('width', slideWidth);
+      this.config.track.css('width', slideWidth * howMuchSlides);
+      this.setWayPoints(slideWidth);
+    },
+    
+    setWayPoints: function (slideWidth) {
+      var total = 0;
+      this.config.wayPoints = [];
+
+      for(var i = 0; i < this.config.video.length; i++) {
+        total += slideWidth;
+        this.config.wayPoints.push(total - slideWidth);
+      }
+    },
+    
+    goToSlide: function (index) {
+      var waypoint = this.config.wayPoints[index];
+      this.config.currentIndex = index;
+      this.config.track.css({
+        '-webkit-transform': 'translate3d(-' + waypoint + 'px,0,0)',
+        '-moz-transform': 'translate3d(-' + waypoint + 'px,0,0)',
+        'transform': 'translate3d(-' + waypoint + 'px,0,0)'
+      });
+    },
+    
+    goRight: function () {
+      var maxIndex = this.config.wayPoints.length - this.config.slidesToShow(),
+          index = (this.config.currentIndex + 1 > maxIndex) ? maxIndex : this.config.currentIndex + 1;
+      this.goToSlide(index);
+    },
+
+    goLeft: function () {
+      var index = (this.config.currentIndex <= 0) ? this.config.currentIndex : this.config.currentIndex - 1;
+      this.goToSlide(index);
+    },
+    
+    bindControls: function () {
+      var that = this;
+      this.config.controls.on('click', function (e) {
+        e.preventDefault();
+        if(this.dataset.dir === 'right') {
+          that.goRight();
+        } else {
+          that.goLeft();
+        }
+      });
+    },
+
+    initRotator: function () {
+      this.bindResizeEvent();
+      this.setTrackWidth();
+      this.bindControls();
     },
     
     init: function() {
@@ -138,7 +218,7 @@
       this.config.scLink.on('click', function (e) {
         e.preventDefault();
         that.clearActive();
-        $(this).parent().addClass('active')
+        $(this).parent().addClass('active');
         src = this.getAttribute('href');
         that.config.iframe.attr('src', src);
       });
